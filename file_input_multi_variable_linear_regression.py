3a5eec86ffa4520442ca82e8e5bf129e1008d68a
songpang <scc6920@naver.com>
2018-08-08 16:44:24 +0900

    ptyhon_tensorflow first_commit

--- file_input_multi_variable_linear_regression.py
+++ file_input_multi_variable_linear_regression.py
@@ -11,25 +11,30 @@ print(x_data.shape, x_data, len(x_data))
 print(y_data.shape, y_data, len(y_data))
 
 
-# X = tf.placeholder(tf.float32, shape=[None, 3])
-# Y = tf.placeholder(tf.float32, shape=[None, 1])
-#
-# W = tf.Variable(tf.random_normal([3, 1]), name= 'weight')
-# b = tf.Variable(tf.random_normal([1]), name= 'bias')
-#
-# hypothesis = tf.matmul(X, W) + b
-#
-# cost = tf.reduce_mean(tf.square(hypothesis - Y))
-#
-# optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
-# train = optimizer.minimize(cost)
-#
-# sess = tf.Session()
-# sess.run(tf.global_variables_initializer())
-#
-# for step in range(2001):
-#     sess.run(train, feed_dict={X:x_data, Y:y_data})
-#     if step % 20 == 0:
-#         print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}))
-#
-# print(sess.run(hypothesis, feed_dict={X:x_data, Y:y_data}))
+X = tf.placeholder(tf.float32, shape=[None, 3])
+Y = tf.placeholder(tf.float32, shape=[None, 1])
+
+W = tf.Variable(tf.random_normal([3, 1]), name= 'weight')
+b = tf.Variable(tf.random_normal([1]), name= 'bias')
+
+hypothesis = tf.matmul(X, W) + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+train = optimizer.minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(2001):
+    sess.run(train, feed_dict={X:x_data, Y:y_data})
+    if step % 20 == 0:
+        print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}))
+
+x_data = [[73.,80.,75.],
+          [93.,88.,93.],
+          [89.,91.,90.],
+          [96.,98.,100.],
+          [73.,66.,70.]]
+print(sess.run(hypothesis, feed_dict={X:x_data}))
