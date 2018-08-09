5630ca1d1db71b41005375658419f57bb051dccc
songpang <scc6920@naver.com>
2018-08-09 11:18:39 +0900

    sigmoid function(plt.show)

--- new_prac_1.py
+++ new_prac_1.py
@@ -1,3 +1,18 @@
-a = 12, 3, 5, 7;
-a[0] = 8
-print(a[0])
+# new_prac.py
+
+import tensorflow as tf
+import numpy as np
+
+xy = np.loadtxt('Data/cars.csv', delimiter=',')
+x = xy[0]
+y = xy[1]
+
+X = tf.placeholder(tf.float32, shape=None)
+Y = tf.placeholder(tf.float32, shape=None)
+
+W = tf.Variable(tf.random_normal([1]), name='weight')
+b = tf.Variable(tf.random_normal([1]), name='bias')
+
+hypothesis = W * X + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
---
+++ sigmoid.py
@@ -0,0 +1,27 @@
+# sigmoid.py
+
+import math
+import matplotlib.pyplot as plt
+
+def sigmoid(z):
+    return 1./(1.+ math.e**-z)
+
+# print(math.e) # e = 2.718281828459045
+
+print(sigmoid(-100))
+print(sigmoid(-10))
+print(sigmoid(0)) # 0.5
+print(sigmoid(10))
+print(sigmoid(100))  # 37부터 1으로 수렴
+
+xx, yy = [], []
+
+for i in range(-100,101):
+    n = sigmoid(i/10
+                )
+
+    xx.append(i/10)
+    yy.append(n)
+
+plt.plot(xx, yy, 'ro')
+plt.show()
